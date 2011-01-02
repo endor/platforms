44724c7306986a5df82f320f31899f625bd9c34d
endor <fp@notjusthosting.com>
2011-01-02 15:34:58 +0100

    add basic sammy app

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,13 @@
+var skeleton.app = $.sammy(function() {
+  this.use(Sammy.Mustache);
+  this.use(Sammy.NestedParams);
+
+  this.get('#/users/new', function() {
+    this.render('views/users/new.mustache');
+  });
+});
+
 $(function() {
+  skeleton.app.run('#/');
+  skeleton.app.trigger('init');
 });
