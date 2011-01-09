3b613f577693a13259058a82c810a3929ca39d8e
Alexander Lang <alex@upstream-berlin.com>
2011-01-09 13:49:01 +0100

    moved everything into lib

--- server.js
+++ server.js
@@ -4,46 +4,22 @@ require.paths.unshift('lib');
 
 var Skeleton = require('./lib/skeleton'),
   User = require('models/user'),
-  QuestionProvider = require('models/question_provider'),
   sys = require('sys');
 
 App = function() {};
-App.session = require('./app/actions/session');
+App.session = require('actions/session');
+App.user = require('actions/user');
+App.question = require('actions/question');
+
 App.prototype = new Skeleton();
 App.prototype.initializeRoutes = function(app) {
   App.session(app);
+  App.user(app);
+  App.question(app);
   
   app.get('/', function(req, res) {
     res.redirect('index.html');
   });
-  
-  app.post('/users', function(req, res) {
-    var user = User.fromParams(req.body.user);
-    if(user.valid()) {
-      app.db.saveDoc(user.toDoc(), function(err, ok) {
-        if(err) {
-          if(err.error == 'conflict') {
-            res.send({user: {username: ['is already taken.']}}, 422);
-          } else {
-            res.send(err);
-          }
-        } else {
-          req.session.user_id = user.toDoc()._id;
-          res.send(201);
-        }
-      });
-    } else {
-      res.send({user: user.errors}, 422);
-    };
-  });
-
-  app.get('/questions', function(req, res) {
-    var questions = QuestionProvider.read('questions.txt');
-    
-    res.send(questions, 200);
-  });
-  
-  
   app.handleError = function(res, callback) {
     return function() {
       var args = Array.prototype.slice.call(arguments); 
