c2efd57a2b00dee137611175bf728dac2651fb99
Alexander Lang <alex@upstream-berlin.com>
2011-01-09 14:20:17 +0100

    encrypt passwords

--- test/user_test.js
+++ test/user_test.js
@@ -2,6 +2,9 @@ var sys = require('sys'),
   User = require('../lib/models/user');
   
 module.exports = {
+  'User.toId should prefix the username': function(assert) {
+    assert.equal('user-alex', User.toId('alex'));
+  },
   'User should be invalid with invalid email': function(assert) {
     var user = User.fromParams({email: 'joe'});
     assert.ok(!user.valid());
@@ -16,6 +19,22 @@ module.exports = {
     var user = User.fromParams({email: 'joe@doe.com', username: 'joe', town: 'Berlin',
       full_name: 'joe doe', life_motto: 'go with the flow', password: 'test'});
     assert.ok(user.valid());
+  },
+  'User.fromParams should encrypt password': function(assert) {
+    var user = User.fromParams({password: 'test'});
+    assert.ok(user.encrypted_password.length > 0);
+  },
+  'User.toDoc should remove the unencrypted password': function(assert) {
+    var user = User.fromParams({password: 'test'});
+    assert.ok(user.toDoc().password == undefined);
+  },
+  'User#authenticate should return true if password matches': function(assert) {
+    var user = User.fromParams({password: 'test'});
+    assert.ok(user.authenticate('test'));
+  },
+  'User#authenticate should return false if password doesn\'t match': function(assert) {
+    var user = User.fromParams({password: 'test'});
+    assert.ok(!user.authenticate('xyz'));
   }
 };
 
