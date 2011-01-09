04713d64f45b5f4f75f208fc426082e664f3eb76
endor <fp@notjusthosting.com>
2011-01-09 15:13:39 +0100

    only hide/show login/logout after getting the session from the server

--- public/js/filters/session_filter.js
+++ public/js/filters/session_filter.js
@@ -2,14 +2,14 @@ skeleton.SessionFilter = function() {
   if(!skeleton.current_user) {
     this.get('/session', null, function(user) {
       skeleton.current_user = user;
+      
+      if(skeleton.current_user) {
+        $('#login').hide()
+        $('#logout').show()
+      } else {
+        $('#logout').hide()
+        $('#login').show()      
+      }
     });
-  }
-  
-  if(skeleton.current_user) {
-    $('#login').hide()
-    $('#logout').show()
-  } else {
-    $('#logout').hide()
-    $('#login').show()      
   }
 }
