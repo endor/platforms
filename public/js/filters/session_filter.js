144ba945c5321411bb047c1bf57b821bc762ca24
endor <fp@notjusthosting.com>
2011-01-09 17:30:52 +0100

    filter allowed routes more specifically in before filter

--- public/js/filters/session_filter.js
+++ public/js/filters/session_filter.js
@@ -1,24 +1,35 @@
-skeleton.SessionFilter = function(context) {
-  if(!skeleton.current_user) {
-    if(['#/users/new', '#/users', '#/session/new', '#/session'].indexOf(context.path) >= 0) {
-      $('#logout').hide();
-      $('#login').show();      
-    } else {
-      context.get('/session', null, function(user) {
-        skeleton.current_user = user;
-        $('#login').hide();
-        $('#logout').show();
-        skeleton.app.runRoute(context.verb, context.path, context.params, context.target);
-      }, function() {
-        context.redirect('#/session/new');
+skeleton.SessionFilter = (function() {
+  var allowed_routes = [
+    {verb: 'get', path: '#/users/new'},
+    {verb: 'post', path: '#/users'},
+    {verb: 'get', path: '#/session/new'},
+    {verb: 'put', path: '#/session'}
+  ];
+  
+  return function(context) {
+    if(!skeleton.current_user) {
+      if(_(allowed_routes).select(function(obj) {
+        return obj.path == context.path && obj.verb == context.verb;
+      }).length > 0) {
         $('#logout').hide();
-        $('#login').show();
-      });
+        $('#login').show();      
+      } else {
+        context.get('/session', null, function(user) {
+          skeleton.current_user = user;
+          $('#login').hide();
+          $('#logout').show();
+          skeleton.app.runRoute(context.verb, context.path, context.params, context.target);
+        }, function() {
+          context.redirect('#/session/new');
+          $('#logout').hide();
+          $('#login').show();
+        });
 
-      return false;
+        return false;
+      }
+    } else {
+      $('#login').hide();
+      $('#logout').show();    
     }
-  } else {
-    $('#login').hide();
-    $('#logout').show();    
-  }
-}
+  };
+})();
