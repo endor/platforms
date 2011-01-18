b59997c42e366e92c9b59dbd6474ac89c766d048
endor <fp@notjusthosting.com>
2011-01-18 11:37:56 +0100

    added basic list of conferences

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,9 @@ cap.app = $.sammy('body', function() {
   
   this.get('#/', function(context) {
     context.get('/categories', function(categories) {
-      context.partial('views/start.mustache', {categories: categories});
+      context.get('/conferences', function(conferences) {
+        context.partial('views/start.mustache', {categories: categories, conferences: conferences});        
+      });
     });
   });
 });
