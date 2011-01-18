0c6a7c92c0765caf372bd8e262a0034863c86339
Alexander Lang <alex@upstream-berlin.com>
2011-01-18 15:42:20 +0100

    fixed date format

--- vows/controllers/conferences_controller-test.js
+++ vows/controllers/conferences_controller-test.js
@@ -8,41 +8,40 @@ vows_http.initialize(3001, '127.0.0.1')
 vows.
   describe('ConferencesController').
   addBatch({
-    'create': {
-      'with valid conference': {
-        topic: function () {
-          var callback = this.callback;
-          
-          vows_http.post('/reset', function() {
-            vows_http.post('/ws/conferences', callback, {name: 'tech', startdate: '02.03.2011', enddate: '04.03.2011', categories: [{id: 'tech-category'}]});
-          });
-        },
-
-        'should return 200': function (error, response) {
-          assert.equal(response.statusCode, 200);
-        },
-        'should return the new conference': function(error, response) {
-          assert.isTrue(response.body.version.length > 0);
-          delete response.body.version;
-          assert.deepEqual(response.body, {name: 'tech', id: 'conference-tech', startdate: '02.03.2011', enddate: '04.03.2011', categories: [{id: 'tech-category'}]});
-        }
+    'create with valid conference': {
+      topic: function () {
+        var callback = this.callback;
+        
+        vows_http.post('/reset', function() {
+          vows_http.post('/ws/conferences', callback, {name: 'tech', startdate: '20110302', enddate: '20110304', categories: [{id: 'tech-category'}]});
+        });
       },
-      'with invalid conference': {
-        topic: function () {
-          var callback = this.callback;
-          
-          vows_http.post('/reset', function() {
-            vows_http.post('/ws/conferences', callback, {name: ''});
-          });
-        },
 
-        'should return 400': function (error, response) {
-          assert.equal(response.statusCode, 400);
-        },
+      'should return 200': function (error, response) {
+        assert.equal(response.statusCode, 200);
       },
-      'without permission': {
-        // XXX
+      'should return the new conference': function(error, response) {
+        assert.isTrue(response.body.version.length > 0);
+        delete response.body.version;
+        assert.deepEqual(response.body, {name: 'tech', id: 'conference-tech', startdate: '20110302', enddate: '20110304', categories: [{id: 'tech-category'}]});
       }
+    }
+  }).addBatch({
+    'create with invalid conference': {
+      topic: function () {
+        var callback = this.callback;
+        
+        vows_http.post('/reset', function() {
+          vows_http.post('/ws/conferences', callback, {name: ''});
+        });
+      },
+
+      'should return 400': function (error, response) {
+        assert.equal(response.statusCode, 400);
+      },
+    },
+    'without permission': {
+      // XXX
     }
   }).
   addBatch({
@@ -54,8 +53,8 @@ vows.
           vows_http.post('/ws/conferences', function() {
             vows_http.post('/ws/conferences', function() {
               vows_http.get('/ws/conferences', callback);
-            }, {name: 'nature', startdate: '02.03.2011', enddate: '04.03.2011', categories: [{id: 'nature-category'}]});            
-          }, {name: 'tech', startdate: '02.03.2011', enddate: '04.03.2011', categories: [{id: 'tech-category'}]});
+            }, {name: 'nature', startdate: '20110302', enddate: '20110304', categories: [{id: 'nature-category'}]});            
+          }, {name: 'tech', startdate: '20110302', enddate: '20110304', categories: [{id: 'tech-category'}]});
         });
       },
       
