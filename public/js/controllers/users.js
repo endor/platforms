f10144df480837679abd003512c20c114c84a828
Alexander Lang <alex@upstream-berlin.com>
2011-01-18 22:22:23 +0100

    added test for session controller

--- public/js/controllers/users.js
+++ public/js/controllers/users.js
@@ -1,6 +1,6 @@
 cap.Users = function(app) {
   app.post('#/users', function(context) {
-    context.post('/ws/users', context.params.user, function(user) {
+    context.post('/ws/members', context.params.user, function(user) {
       context.flash('Welcome ' + user.fullname);
       cap.current_user = user
       context.redirect('#/members/' + user.username);
--- vows/controllers/session_controller-test.js
+++ vows/controllers/session_controller-test.js
@@ -12,20 +12,19 @@ vows.
         topic: function () {
           var callback = this.callback;
           
-          vows_http.post('/users', function(e, res) {
-            vows_http.put('/session', callback, {session: {username: 'joe', password: 'test'}})
-          }, {user: {email: 'joe@doe.com', username: 'joe', town: 'Berlin',
-              fullname: 'joe doe', country: 'Germnay', password: 'test'}})
+          vows_http.post('/reset', function(err, res) {
+            vows_http.post('/ws/members', function(e, res) {
+              vows_http.put('/session', callback, {session: {username: 'joe', password: 'test'}})
+            }, {user: {email: 'joe@doe.com', username: 'joe', town: 'Berlin',
+                fullname: 'joe doe', country: 'Germnay', password: 'test'}})
+          });
         },
-
+        'should return 200': function(err, res) {
+          assert.equal(res.statusCode, 200);
+        },
         'should return the session id in a cookie': function (error, response) {
           assert.match(response.headers['set-cookie'], /_node=[^;]+;/);
-          /**
-            TODO:
-              stub successful authentication or cleanup database
-              check for error codes when logging in with wrong details
-          */
-        }        
+        }
       }
     }
   }).
